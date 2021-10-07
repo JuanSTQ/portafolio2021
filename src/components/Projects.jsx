c9248b8bf8399fdc23415a40e44d33591d5d68c3
juanstq <juanstq20@gmail.com>
2021-10-07 02:15:10 -0500

    add movies project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import confStore from '../images/enzardev.png'
 import weatherApp from '../images/weather.png'
 import bateria from '../images/bateria.png'
 import rickandmorty from '../images/rickandmorty.png'
+import benturi from '../images/benturi.png'
 const Projects = () => {
   return (
     <>
@@ -11,18 +12,18 @@ const Projects = () => {
         <h2 id="projects" className="projects__title">Projects</h2>
         <div className="container__projects">
           <section className="card_project">
-            <img src={confStore} alt="Proyecto" />
-            <p className="card_tecnologies">html, css, Javascript, ReactJS, firebase</p>
+            <img src={benturi} alt="Proyecto" />
+            <p className="card_tecnologies">html, css, Javascript, ReactJS, Nodejs, Redux</p>
             <div className="overlay-project">
-              <p>Una web para comprar articulos</p>
-              <a href="https://enzardev.xyz/" target="_blank">Visit Website</a>
+              <p>Una web para ver peliculas</p>
+              <a href="https://benturi.herokuapp.com/" target="_blank">Visit Website</a>
             </div>
           </section>
           <section className="card_project">
             <img src={weatherApp} alt="Proyecto" />
             <p className="card_tecnologies">html, css, Javascript</p>
             <div className="overlay-project">
-              <p>Una web para comprar articulos</p>
+              <p>Una web para buscar el clima de tu ciudad</p>
               <a href="https://enzardev.xyz/" target="_blank">Visit Website</a>
             </div>
           </section>
@@ -30,7 +31,7 @@ const Projects = () => {
             <img src={bateria} alt="Proyecto" />
             <p className="card_tecnologies">html, css, Javascript</p>
             <div className="overlay-project">
-              <p>Una web para comprar articulos</p>
+              <p>Una web para tocar musica</p>
               <a href="https://enzardev.xyz/" target="_blank">Visit Website</a>
             </div>
           </section>
@@ -38,7 +39,7 @@ const Projects = () => {
             <img src={rickandmorty} alt="Proyecto" />
             <p className="card_tecnologies">html, css, Javascript</p>
             <div className="overlay-project">
-              <p>Una web para comprar articulos</p>
+              <p>Una web para ver personajes de rick and morty</p>
               <a href="https://enzardev.xyz/" target="_blank">Visit Website</a>
             </div>
           </section>
